67488d11c8c653d7791594bc645cde871ae214eb
pt960324 <18811444781@163.com>
2016-05-23 20:50:35 +0800

    201605232050p

---
+++ admin/yii/backend/controllers/BaseController.php
@@ -0,0 +1,15 @@
+<?php
+namespace backend\controllers;
+use Yii;
+
+use yii\web\Controller;
+class BaseController extends \yii\web\Controller
+{
+	public function actionJqueryui()
+    {
+		echo 111;
+    }
+	public function actionNestablelist(){
+		echo 222;
+	}
+}
