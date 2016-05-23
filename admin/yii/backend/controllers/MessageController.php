baa37d79f6b24050408a11ad349e89d9a40f92ba
liyajiao <18710127273@163.com>
2016-05-23 21:54:03 +0800

    fd

--- admin/yii/backend/controllers/MessageController.php
+++ admin/yii/backend/controllers/MessageController.php
@@ -1,12 +1,33 @@
 <?php
 
 namespace backend\controllers;
-
+use Yii;
 class MessageController extends \yii\web\Controller
 {
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
+	//审核已完成列表(包括通过的和没通过的)
+
+	public function actionList(){
+		$session=Yii::$app->session;
+		$name=$session->get('name');
+		$sql=Yii::$app->db->createCommand("select * from report where r_state=2 or r_state=3");
+		$re=$sql->queryAll();
+		return $this->renderPartial('message_list',array('name'=>$name,'content'=>$re));
+	}
+
+	public function actionCheckill(){
+		$session=Yii::$app->session;
+		$name=$session->get('name');
+		$sql=Yii::$app->db->createCommand("select * from report where r_state=0");
+		$re=$sql->queryAll();
+		return $this->renderPartial('checkill',array('name'=>$name,'content'=>$re));
+	}
+
+	public function actionChecking(){
+		$session=Yii::$app->session;
+		$name=$session->get('name');
+		$sql=Yii::$app->db->createCommand("select * from report where r_state=1");
+		$re=$sql->queryAll();
+		return $this->renderPartial('checking',array('name'=>$name,'content'=>$re));
+	}
 
 }
