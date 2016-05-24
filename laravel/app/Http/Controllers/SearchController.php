5999fc4a96efdbdc70f7d3e8e1424987b77cc317
gaojiahao1 <15210168509@163.com>
2016-05-24 09:01:08 +0800

    5.24

--- laravel/app/Http/Controllers/SearchController.php
+++ laravel/app/Http/Controllers/SearchController.php
@@ -6,7 +6,7 @@ use Request;
 header('content-type:text/html;charset=utf-8');
 class SearchController extends Controller {
 	public function HomepageSearch(){
-		$name=$_COOKIE['name'];
+		@$name=$_COOKIE['name'];
 		$city=Request::get('city');
 		$time=Request::get('time');
 		$db=DB::table('house')->join('picture', 'house.h_id', '=', 'picture.pi_h_id')->where('h_city','like','%'.$city.'%','and','h_state','1')->get();
