191fc138e94b825fc7b48f390465cfeeb937e611
renpengdong <1317842223@qq.com>
2016-05-24 10:33:03 +0800

    1111

--- laravel/app/Http/Controllers/LoginController.php
+++ laravel/app/Http/Controllers/LoginController.php
@@ -13,7 +13,8 @@ class LoginController extends Controller {
 		$u_pwd=Request::input('u_pwd');
 		$u_tel=Request::input('u_tel');
 		//echo $u_pwd;
-		$db=DB::insert("insert into user(u_name,u_pwd,u_tel) values('$u_name','$u_pwd','$u_tel')");
+		$u_time=date('Y-m-d H:i:s');
+		$db=DB::insert("insert into user(u_name,u_pwd,u_tel,u_time) values('$u_name','$u_pwd','$u_tel','$u_time')");
 		if($db)
 		{
 			echo "<script>alert('注册成功');location.href='/database/ninegroups/laravel/public/index'</script>";
