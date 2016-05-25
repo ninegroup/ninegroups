0d766fb0bccacac72712eff2c5d2c9cc349160f5
liujiahua <18811372835@163.com>
2016-05-25 19:52:25 +0800

    a

--- admin/yii/backend/views/housing/houselist.php
+++ admin/yii/backend/views/housing/houselist.php
@@ -542,7 +542,7 @@ use yii\widgets\LinkPager;
 							</h1>
 						</div><!-- /.page-header -->
 						<input type="text" name="sou" id='sous' value="<?php $s=$sou?$sou:''; echo $s;?>">		
-						<button class="btn btn-xs btn-pink" onclick="sou()">搜索<?php $s=$sou?$sou:''; echo $s;?></button>
+						<button class="btn btn-xs btn-pink" onclick="sou()">搜索</button>
 						<div class="row">
 							<div class="col-xs-12">
 								<!-- PAGE CONTENT BEGINS -->
