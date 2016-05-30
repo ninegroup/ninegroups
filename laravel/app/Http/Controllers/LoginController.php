ac7d2b973104f932f9bb94c853d892823a30fc75
gaojiahao1 <15210168509@163.com>
2016-05-30 08:44:45 +0800

    232

--- laravel/app/Http/Controllers/LoginController.php
+++ laravel/app/Http/Controllers/LoginController.php
@@ -68,7 +68,7 @@ class LoginController extends Controller {
 	{
 		if(empty($_COOKIE['name']))
 		{
-			echo "<script>alert('请先登录');location.href='index'</script>";
+			echo"<script>alert('请先登录');history.go(-1);</script>"; 
 		}else{
 			
 			return view("publish");
