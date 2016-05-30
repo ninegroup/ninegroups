d198903c1577d6c13cbd8b4df5b9a818a9f67f5d
liujiahua <18811372835@163.com>
2016-05-30 15:22:35 +0800

    a

--- laravel/app/Http/Controllers/DetailsController.php
+++ laravel/app/Http/Controllers/DetailsController.php
@@ -31,41 +31,28 @@ class DetailsController extends Controller {
 	{
 		static $pic='';
 		$id=Request::get('id');
-<<<<<<< HEAD
-		$arr=DB::table('house')->where('h_id',$id)->first();	
-		// var_dump($arr);die;
-=======
-<<<<<<< HEAD
-
 		$arr=DB::table('house')->where('h_id',$id)->first();
-=======
->>>>>>> 6788bc0f6acd3442c48a3b2a29ce7a36df0b3eba
-		$arr=DB::table('house')->where('h_id',$id)->first();
-		$picture=DB::table('picture')->where('pi_h_id',$id)->where('pi_state',2)->get();
-		$arr=DB::table('house')->where('h_id',$id)->first();	
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 3a9aa5c1aafb29550d44d30da4a404b81edfd4bb
->>>>>>> 20a30d3f39d54eab25d394f279d8ad37e033f337
->>>>>>> 6788bc0f6acd3442c48a3b2a29ce7a36df0b3eba
-		$picture=DB::table('picture')->where('pi_h_id',$id)->get();
 		$u_id=$arr->u_id;
 		//echo $u_id;die;
+		$picture=DB::table('picture')->where('pi_h_id',$id)->where('pi_state',2)->get();
 		$res=DB::table('user')->where('u_id',$u_id)->first();
-		$com=DB::table('house')->where('u_id',$u_id)->limit(3)->get();
+		$coms=DB::table('house')->where('u_id',$u_id)->limit(3)->get();
+		//var_dump($com);die;
+		$h_id=$coms[0]->h_id;	
+		$com=DB::table('house')
+		->join('picture', 'house.h_id', '=', 'picture.pi_h_id')
+		->where('h_state','1')
+		->where('pi_state','2')
+		->get();
+		//var_dump($com);die;
 		$data=array(
 			'arr'=>$arr,
 			'picture'=>$picture,
 			'res'=>$res,
 			'com'=>$com
 		);
-		//var_dump($com);die;
 		return view('details',$data);
-<<<<<<< HEAD
 
-=======
->>>>>>> 20a30d3f39d54eab25d394f279d8ad37e033f337
 	}
 
 }
--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -68,6 +68,8 @@ Route::any('/Login/publish2', [ 'as' => 'Login/publish2', 'uses' => "LoginContro
 Route::any('/Search/HomepageSearch', [ 'as' => 'Search/HomepageSearch', 'uses' => "SearchController@HomepageSearch"]);
 //房源具体信息页面
 Route::any('/Details/Index', [ 'as' => 'Details/Index', 'uses' => "DetailsController@Index"]);
-
+Route::any('/Details/HouseReserve', [ 'as' => 'Details/HouseReserve', 'uses' => "DetailsController@HouseReserve"]);
 //发布房源login/addhouse1
 Route::any('addhouse1','LoginController@addhouse1');
+
+
